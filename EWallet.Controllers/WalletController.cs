7aa12b5004630201d2b3dc2edc687e83720028d5
TeaFaris <teafaris@organizatsiya.site>
2023-10-11 19:17:57 +0500

    Added controller and application part

---
+++ EWallet.Controllers/WalletController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EWallet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public sealed class WalletController : ControllerBase
+    {
+        
+    }
+}
