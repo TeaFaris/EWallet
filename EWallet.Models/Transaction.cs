b8aeedf0dc6277a25460b7bd61a1e81ab6d5b1ac
TeaFaris <teafaris@organizatsiya.site>
2023-10-11 15:58:32 +0500

    Wallet and Transaction models were added.

---
+++ EWallet.Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EWallet.Models
+{
+    public sealed class Transaction
+    {
+        [Key]
+        public int Id { get; init; }
+
+        public int WalletId { get; init; }
+        [ForeignKey(nameof(WalletId))]
+        public Wallet Wallet { get; init; }
+
+        public DateTime Issued { get; init; }
+
+        public decimal Amount { get; init; }
+    }
+}
---
+++ EWallet.Models/Wallet.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EWallet.Models
+{
+    public sealed class Wallet
+    {
+        [Key]
+        public int Id { get; init; }
+
+        public decimal Balance { get; init; }
+
+        public List<Transaction> Transactions { get; init; }
+    }
+}
